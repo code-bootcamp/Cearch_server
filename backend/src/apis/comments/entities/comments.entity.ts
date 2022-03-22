9457857149a8213289048c773018d038e0c80f7f
SeungHyeon12 <tmdgustn06@naver.com>
2022-03-22 12:17:36 +0900

    Mangosix : conflict error fixed

--- backend/src/apis/comments/entities/comments.entity.ts
+++ backend/src/apis/comments/entities/comments.entity.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { QtBoard } from 'src/apis/QtBoard/entities/qt.entity';
 import { User } from 'src/apis/user/entities/user.entity';
 import {
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,3 @@
-
 import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -10,14 +9,12 @@ import * as redisStore from 'cache-manager-redis-store';
 import { AppService } from './app.service';
 import { CommentsModule } from './apis/comments/comments.module';
 import { LectureProductModule } from './apis/lectureProduct/lectureProduct.module';
-import { LectureProductCategoryModule } from './apis/lectureproductCategory/lectureproductCategory.module';
 import { QtBoardModule } from './apis/QtBoard/QtBoard.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { JwtAccessStrategy } from './common/auth/strategy/access.strategy.jwt';
 import { JwtRefreshStrategy } from './common/auth/strategy/refresh.strategy.jwt';
 import { FileUploadModule } from './apis/file/file.module';
 import { PointModule } from './apis/point/point.module';
-
 
 @Module({
   imports: [
@@ -27,7 +24,6 @@ import { PointModule } from './apis/point/point.module';
     QtBoardModule,
     CommentsModule,
     LectureProductModule,
-    LectureProductCategoryModule,
     FileUploadModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: '/src/grapqhql/schema.gql',
